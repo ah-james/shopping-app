4beaaf851d51a198144bd0e49202cd149d3c3059
ah-james <11ahj11@gmail.com>
2021-05-23 16:48:49 -0400

    STarting Products Overview & Small Changes

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+    primaryColor: '#ff9f1c',
+    secondaryColor: '#2ec4b6'
+}
--- containers/shop/ProductsOverviewContainer.js
+++ containers/shop/ProductsOverviewContainer.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { useSelector } from 'react-redux'
 
 const ProductsOverviewContainer = props => {
+    const products = useSelector(state => state.products.availableProducts)
+    return <FlatList data={products} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
+}
 
+ProductsOverviewContainer.navigationOptions = {
+    headerTitle: 'All Products'
 }
 
 const styles = StyleSheet.create({
