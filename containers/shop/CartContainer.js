9acc7c55bc9e19470eca263a63e7c295eeefd06e
ah-james <11ahj11@gmail.com>
2021-05-23 11:53:47 -0400

    Beginning To Build Container Components

---
+++ containers/shop/CartContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const CartContainer = props => {
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default CartContainer
---
+++ containers/user/EditProductContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const EditProductContainer = props => {
+
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default EditProductContainer
