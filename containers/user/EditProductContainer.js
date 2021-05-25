95e3eab699b60d0e175fb4f49d073ad2321e6e9c
ah-james <11ahj11@gmail.com>
2021-05-25 11:57:32 -0400

    Functional and Styled Edit Product Container

--- containers/user/EditProductContainer.js
+++ containers/user/EditProductContainer.js
@@ -1,12 +1,78 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect, useCallback } from 'react'
+import { View, Text, StyleSheet, ScrollView, TextInput, Platform } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { useSelector } from 'react-redux' 
+
+import CustomHeaderButton from '../../components/UI/CustomHeaderButton'
 
 const EditProductContainer = props => {
+    const productId = props.navigation.getParam('productId')
+    const editedProduct = useSelector(state => state.products.userProducts.find(product => product.id === productId))
 
+    const [title, setTitle] = useState(editedProduct ? editedProduct.title : '')
+    const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '')
+    const [price, setPrice] = useState('')
+    const [description, setDescription] = useState(editedProduct ? editedProduct.description : '')
+
+    const handleSubmit = useCallback(() => {
+        console.log('submit')
+    }, [])
+
+    useEffect(() => {
+        props.navigation.setParams({ 'submit': handleSubmit })
+    }, [handleSubmit])
+
+    return(
+        <ScrollView>
+            <View style={styles.form}>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Title</Text>
+                    <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>URL</Text>
+                    <TextInput style={styles.input} value={imageUrl} onChangeText={text => setImageUrl(text)} />
+                </View>
+                {editedProduct ? null : <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Price</Text>
+                    <TextInput style={styles.input} value={price} onChangeText={text => setPrice(text)} />
+                </View>}
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Description</Text>
+                    <TextInput style={styles.input} value={description} onChangeText={text => setDescription(text)} />
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+
+EditProductContainer.navigationOptions = navData => {
+    const submitFunction = navData.navigation.getParam('submit')
+    return {
+        headerTitle: navData.navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
+        headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item title="Add" iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'} onPress={submitFunction} />
+        </HeaderButtons>,
+    }
 }
 
 const styles = StyleSheet.create({
-
+    form: {
+        margin: 20,
+    },
+    inputContainer: {
+        width: '100%',
+    },
+    label: {
+        fontFamily: 'open-sans-bold',
+        marginVertical: 8,
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 5,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+    },
 })
 
 export default EditProductContainer
