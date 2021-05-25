98c4eea986755572927c53fc6b01a9094b96fe60
ah-james <11ahj11@gmail.com>
2021-05-24 20:00:24 -0400

    Remove Action and Reducer

--- store/actions/cartActions.js
+++ store/actions/cartActions.js
@@ -1,5 +1,12 @@
+import { Platform } from "react-native"
+
 export const ADD_TO_CART = 'ADD_TO_CART'
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 
 export const addToCart = product => {
     return { type: ADD_TO_CART, product: product }
+}
+
+export const removeFromCart = productId => {
+    return {type: REMOVE_FROM_CART, productId: productId}
 }
--- store/reducers/cartReducer.js
+++ store/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../actions/cartActions"
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cartActions"
 import CartItem from '../../models/cart-item'
 
 const initialState = {
@@ -22,7 +22,26 @@ export default (state = initialState, action) => {
                 updateOrNewCartItem = new CartItem(1, productPrice, productTitle, productPrice)
             }
             return {...state, items: {...state.items, [addedProduct.id]: updateOrNewCartItem}, sum: state.sum + productPrice}
-
+        case REMOVE_FROM_CART:
+            const selectedCartItem = state.items[action.productId]
+            const currentQuantity = selectedCartItem.quantity
+            let updatedCartItems
+            if (currentQuantity > 1) {
+                // reduce by 1 instead of erasing
+                // create new CartItem
+                const updatedCartItem = new CartItem(selectedCartItem.quantity - 1, selectedCartItem.productPrice, selectedCartItem.productTitle, selectedCartItem.sum - selectedCartItem.productPrice)
+                updatedCartItems = {...state.items, [action.productId]: updatedCartItem}
+            } else {
+                // spread operator on all items in state
+                updatedCartItems = {...state.items}
+                // delete specific item where action's productId is equal to the key
+                delete updatedCartItems[action.productId]
+            }
+            return {
+                ...state,
+                items: updatedCartItems,
+                sum: state.sum - selectedCartItem.productPrice
+            }
     }
     return state
 }
