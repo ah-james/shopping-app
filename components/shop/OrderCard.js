ae07f9dc4345720e73fb512d7107809248db7550
ah-james <11ahj11@gmail.com>
2021-05-25 19:19:11 -0400

    Adding Custom Card Component

--- components/shop/OrderCard.js
+++ components/shop/OrderCard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
+import Card from '../UI/Card'
 import Colors from '../../constants/Colors'
 import CartCard from './CartCard'
 
@@ -8,26 +9,19 @@ const OrderCard = props => {
     const [showDetails, setShowDetails] = useState(false)
 
     return(
-        <View style={styles.orderItem}>
+        <Card style={styles.orderItem}>
             <View style={styles.infoContainer}>
                 <Text style={styles.total}>{props.totalAmount.toFixed(2)}</Text>
                 <Text style={styles.date}>{props.date}</Text>
             </View>
             <Button color={Colors.primaryColor} title={showDetails ? "Hide Details" : "Show Details"} onPress={() => setShowDetails(prevState => !prevState)} />
             {showDetails && <View style={styles.cartItems}>{props.items.map(item => <CartCard key={item.productId} quantity={item.quantity} title={item.productTitle} amount={item.sum} />)}</View>}
-        </View>
+        </Card>
     )
 }
 
 const styles = StyleSheet.create({
     orderItem: {
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
         margin: 20,
         padding: 10,
         alignItems: 'center'
--- containers/shop/CartContainer.js
+++ containers/shop/CartContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 
+import Card from '../../components/UI/Card'
 import Colors from '../../constants/Colors'
 import CartCard from '../../components/shop/CartCard'
 import * as cartActions from '../../store/actions/cartActions'
@@ -28,12 +29,12 @@ const CartContainer = props => {
 
     return(
         <View style={styles.screen}>
-            <View style={styles.summary}>
+            <Card style={styles.summary}>
                 <Text style={styles.summaryText}>
                     Total: <Text style={styles.sum}>${Math.round(cartSum.toFixed(2) * 100) / 100}</Text>
                 </Text>
                 <Button title="Order Now" color={Colors.secondaryColor} disabled={cartItems.length === 0} onPress={() => { dispatch(ordersActions.addOrder(cartItems, cartSum))}} />
-            </View>
+            </Card>
             <FlatList 
                 data={cartItems} 
                 keyExtractor={item => item.productId} 
@@ -67,13 +68,6 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         marginBottom: 20,
         padding: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
     },
     summaryText: {
         fontFamily: 'open-sans-bold',
